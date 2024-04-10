d37a9f6fbff74f263535fe88f1ebe76ec6a79867
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-11 00:50:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// let randomnum = Math.random();
+const generatenum = Math.floor(Math.random() + 5 * 1);
+const answer = await inquirer.prompt([
+    {
+        name: "guessnumber",
+        type: "input",
+        message: "guess number(limit from 1 to 5 )"
+    }
+]);
+if (answer.guessnumber === generatenum) {
+    console.log("Congrates! you guess a right number");
+}
+else {
+    console.log("Wrong number");
+}
